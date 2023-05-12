6a6e6d6630feea6e34cb88f6daf39e10ea42f6a5
rawankhayat <132397126+rawankhayat@users.noreply.github.com>
2023-05-12 18:16:47 +0300

    Add files via upload

---
+++ lib/profile_page.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProfilePageState();
+  }
+}
+
+class _ProfilePageState extends State<ProfilePage>{
+
+  double  _drawerIconSize = 24;
+  double _drawerFontSize = 17;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+/*      appBar: AppBar(
+        title: Text("Profile Page",
+          style: TextStyle(color: Colors.white,
+              fontWeight: FontWeight.bold
+          ),
+        ),
+        elevation: 0.5,
+        iconTheme: IconThemeData(color: Colors.white),
+        flexibleSpace:Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[Theme.of(context).primaryColor, Theme.of(context).accentColor,]
+              )
+          ),
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.only( top: 16, right: 16,),
+            child: Stack(
+              children: <Widget>[
+                Icon(Icons.notifications),
+                Positioned(
+                  right: 0,
+                  child: Container(
+                    padding: EdgeInsets.all(1),
+                    decoration: BoxDecoration( color: Colors.red, borderRadius: BorderRadius.circular(6),),
+                    constraints: BoxConstraints( minWidth: 12, minHeight: 12, ),
+                    child: Text( '5', style: TextStyle(color: Colors.white, fontSize: 8,), textAlign: TextAlign.center,),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+      drawer: Drawer(
+        child: Container(
+          decoration:BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.0, 1.0],
+                  colors: [
+                    Theme.of(context).primaryColor.withOpacity(0.2),
+                    Theme.of(context).accentColor.withOpacity(0.5),
+                  ]
+              )
+          ) ,
+          child: ListView(
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    stops: [0.0, 1.0],
+                    colors: [ Theme.of(context).primaryColor,Theme.of(context).accentColor,],
+                  ),
+                ),
+                child: Container(
+                  alignment: Alignment.bottomLeft,
+                  child: Text("FlutterTutorial.Net",
+                    style: TextStyle(fontSize: 25,color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+
+
+              ListTile(
+                leading: Icon(Icons.logout_rounded, size: _drawerIconSize,color: Theme.of(context).accentColor,),
+                title: Text('Logout',style: TextStyle(fontSize: _drawerFontSize,color: Theme.of(context).accentColor),),
+                onTap: () {
+                  SystemNavigator.pop();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),*/
+
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+             // height: 100, child: HeaderWidget(100,false,Icons.house_rounded),
+            ),
+
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.fromLTRB(25, 10, 25, 10),
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      border: Border.all(width: 5, color: Colors.white),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(color: Colors.black12, blurRadius: 20, offset: const Offset(5, 5),),
+                      ],
+                    ),
+
+                    child: Icon(Icons.person, size: 80, color: Colors.grey.shade300,),
+                  ),
+
+                  /* SizedBox(height: 20,),
+                  Text('Mr. Donald Trump', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
+                  SizedBox(height: 20,),
+                  Text('Former President', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                  SizedBox(height: 10,),
+*/
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+}
